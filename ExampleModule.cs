8db792fdba4a6544ae381276e11eb98beb875bba
RainOrigami <rain+github@codefreak.net>
2023-08-17 00:05:14 +0200

    Initial examples

---
+++ ExampleModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using BBRAPIModules;
+
+namespace BattleBitExamples;
+
+[RequireModule(typeof(ExampleModuleIntegration))]
+public class ExampleModule : BattleBitModule
+{
+    public ExampleModule(RunnerServer server) : base(server)
+    {
+    }
+
+    private ExampleModuleIntegration exampleModuleIntegration;
+
+    public override void OnModulesLoaded()
+    {
+        this.exampleModuleIntegration = this.Server.GetModule<ExampleModuleIntegration>()!;
+    }
+
+    public override Task OnConnected()
+    {
+        Console.WriteLine("Hello");
+        this.exampleModuleIntegration.Test();
+        return Task.CompletedTask;
+    }
+}
---
+++ ExampleModuleIntegration.cs
@@ -0,0 +1,15 @@
+﻿using BBRAPIModules;
+
+namespace BattleBitExamples;
+
+public class ExampleModuleIntegration : BattleBitModule
+{
+    public ExampleModuleIntegration(RunnerServer server) : base(server)
+    {
+    }
+
+    public void Test()
+    {
+        this.Server.SayToChat("Test");
+    }
+}
